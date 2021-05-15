bac9a9e053e2a80de0bc1ee4cb5a1f3c1b66d0e2
Abhishek Adhikari <abhiayush23@gmail.com>
2021-05-15 19:17:07 +0530

    Replaced Ternary with OR and AND

--- src/auth.ts
+++ src/auth.ts
@@ -1,26 +1,23 @@
-import { User } from "./models/User";
-import jwt from "jsonwebtoken";
+import { User } from './models/User';
+import jwt from 'jsonwebtoken';
 
 export const createAccessToken = (user: User) => {
-    return jwt.sign(
-        { userId: user.id },
-        process.env.JWT_ACCESS_SECRET
-            ? process.env.JWT_ACCESS_SECRET
-            : "somearbitrarilylongsecretthatyoushouldntcareabouttoomuch",
-        {
-            expiresIn: "15m",
-        }
-    );
+	return jwt.sign(
+		{ userId: user.id },
+		process.env.JWT_ACCESS_SECRET || 'somearbitrarilylongsecretthatyoushouldntcareabouttoomuch',
+		{
+			expiresIn: '15m'
+		}
+	);
 };
 
 export const createRefreshToken = (user: User) => {
-    return jwt.sign(
-        { userId: user.id },
-        process.env.JWT_REFRESH_SECRET
-            ? process.env.JWT_REFRESH_SECRET
-            : "somearbitrarilylongsecretthatyoushouldntcareabouttoomuchorelseyoulljustwasteyourtime",
-        {
-            expiresIn: "7d",
-        }
-    );
+	return jwt.sign(
+		{ userId: user.id },
+		process.env.JWT_REFRESH_SECRET ||
+			'somearbitrarilylongsecretthatyoushouldntcareabouttoomuchorelseyoulljustwasteyourtime',
+		{
+			expiresIn: '7d'
+		}
+	);
 };
